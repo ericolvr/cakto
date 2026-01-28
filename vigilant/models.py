953e9a813caefdc5b328f130418d38b2ac4fbd22
ericolvr <ericolvr@gmail.com>
2026-01-28 08:31:32 -0300

    Vigilante user adicionado

--- vigilant/models.py
+++ vigilant/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.conf import settings
+
 
 class Vigilant(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='vigilant'
+    )
     name = models.CharField(max_length=100)
     mobile = models.CharField(max_length=15)
     
