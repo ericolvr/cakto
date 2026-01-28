be28d3f971170dc06d0916e3a45220c0c5fba524
ericolvr <ericolvr@gmail.com>
2026-01-28 08:31:42 -0300

    base urls

--- project/urls.py
+++ project/urls.py
@@ -1,22 +1,25 @@
 """
 URL configuration for project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from branch.views import BranchListCreateAPIView, BranchDetailAPIView
+from vigilant.views import VigilantListCreateAPIView, VigilantDetailAPIView
+from history.views import HistoryListCreateAPIView, HistoryDetailAPIView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Branch endpoi
+    path('api/branches/', BranchListCreateAPIView.as_view(), name='branch-list-create'),
+    path('api/branches/<int:pk>/', BranchDetailAPIView.as_view(), name='branch-detail'),
+    
+    # Vigilant endpoints
+    path('api/vigilants/', VigilantListCreateAPIView.as_view(), name='vigilant-list-create'),
+    path('api/vigilants/<int:pk>/', VigilantDetailAPIView.as_view(), name='vigilant-detail'),
+    
+    # History endpoints
+    path('api/histories/', HistoryListCreateAPIView.as_view(), name='history-list-create'),
+    path('api/histories/<int:pk>/', HistoryDetailAPIView.as_view(), name='history-detail'),
 ]
