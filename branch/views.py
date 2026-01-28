cd71d5c03f048ec1e2cc9b72f3cf525c2e092f47
ericolvr <ericolvr@gmail.com>
2026-01-28 08:30:37 -0300

    Branch Views

--- branch/views.py
+++ branch/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import Branch
+from .serializers import BranchSerializer
 
-# Create your views here.
+
+class BranchListCreateAPIView(APIView):
+    """
+    GET: Lista todas as branches
+    POST: Cria uma nova branch
+    """
+    
+    def get(self, request):
+        branches = Branch.objects.all()
+        serializer = BranchSerializer(branches, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = BranchSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BranchDetailAPIView(APIView):
+    """
+    GET: Retorna detalhes de uma branch específica
+    PUT: Atualiza completamente uma branch
+    PATCH: Atualiza parcialmente uma branch
+    DELETE: Remove uma branch
+    """
+    
+    def get(self, request, pk):
+        branch = get_object_or_404(Branch, pk=pk)
+        serializer = BranchSerializer(branch)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        branch = get_object_or_404(Branch, pk=pk)
+        serializer = BranchSerializer(branch, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        branch = get_object_or_404(Branch, pk=pk)
+        serializer = BranchSerializer(branch, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        branch = get_object_or_404(Branch, pk=pk)
+        branch.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
