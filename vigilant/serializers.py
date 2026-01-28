676e3643fb45d7ece9d319e5acb361221272ec62
ericolvr <ericolvr@gmail.com>
2026-01-28 08:31:52 -0300

    Vigilant Serializer

--- vigilant/serializers.py
+++ vigilant/serializers.py
@@ -3,7 +3,9 @@ from .models import Vigilant
 
 
 class VigilantSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(source='user.id', read_only=True)
+    
     class Meta:
         model = Vigilant
-        fields = ['id', 'name', 'mobile']
-        read_only_fields = ['id']
+        fields = ['id', 'user_id', 'name', 'mobile']
+        read_only_fields = ['id', 'user_id']
