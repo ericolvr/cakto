1585c4423d2c2f8448707ddace2a0548b33e632f
ericolvr <ericolvr@gmail.com>
2026-01-28 08:16:27 -0300

    definição do model History

--- history/models.py
+++ history/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from vigilant.models import Vigilant
+from branch.models import Branch
 
-# Create your models here.
+
+class History(models.Model):
+    vigilant = models.ForeignKey(
+        Vigilant, 
+        on_delete=models.CASCADE, 
+        related_name='histories'
+    )
+    branch = models.ForeignKey(
+        Branch, 
+        on_delete=models.CASCADE, 
+        related_name='histories'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta:
+        verbose_name = 'Histórico'
+        verbose_name_plural = 'Históricos'
+        ordering = ['-created_at']
